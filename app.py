e23d525ea589a9935b5cf88fe168b2cd0a8a393c
mianjunaid1223 <mianjunaid2312@gmail.com>
2024-04-23 09:20:20 -0700

    main

--- app.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-from Bard import Chatbot #pip install GoogleBard
-
-Secure_1PSID = environ.get("BARD__Secure_1PSID")
-Secure_1PSIDTS = environ.get("BARD__Secure_1PSIDTS")
-chatbot = Chatbot(Secure_1PSID, Secure_1PSIDTS)
-
-chatbot.ask("Hello, how are you?")
